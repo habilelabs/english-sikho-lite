2a4efee6de91931501657cbf1d163362ccf461e8
Shankar Morwal <morwal89@gmail.com>
2016-09-16 17:50:22 +0530

    Deleted views

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,5 @@ module.exports = function (app) {
     app.get('/', app.home);
     app.get('/word/:word', app.wordPage);
     app.get('/browse', app.browsePage);
-	app.get('/search/:word', app.searchPage);
-	
+	app.get('/search/:word', app.searchPage)
 };
